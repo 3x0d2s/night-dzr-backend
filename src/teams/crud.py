6649e9cd3d4d8c260cc14be2c180edcb406179b9
3x0d2s-pc <exodus.yarwood@icloud.com>
2023-05-30 13:27:25 +0300

    Реализовано добавление/удаление участников команды + получение списка участников команды

--- src/teams/crud.py
+++ src/teams/crud.py
@@ -1,12 +1,15 @@
 from sqlalchemy import select, exists
 from sqlalchemy.sql import func
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 from src.teams.models import Team
+from src.auth.models import User
+from src.teams.utils import TeamsUserNotFound
 
 
 async def get_team_data(db: AsyncSession, team_id: int):
     """ Возвращает данные команды.  """
-    query = select(Team).filter(Team.id == team_id)
+    query = select(Team).filter(Team.id == team_id).options(selectinload(Team.users))
     result = await db.execute(query)
     return result.scalars().first()
 
@@ -39,3 +42,22 @@ async def update_team(db: AsyncSession, team: Team, update_data: dict):
         setattr(team, key, value)
     await db.commit()
     return team
+
+
+async def add_user_to_team(db: AsyncSession, team: Team, user_id: int):
+    query = select(User).filter(User.id == user_id)
+    result = await db.execute(query)
+    user = result.scalars().first()
+    team.users.append(user)
+    await db.commit()
+
+
+async def remove_user_from_team(db: AsyncSession, team: Team, user_id: int):
+    flag = False
+    for team_user in team.users:
+        if team_user.id == user_id:
+            team.users.remove(team_user)
+            flag = True
+    if not flag:
+        raise TeamsUserNotFound
+    await db.commit()
